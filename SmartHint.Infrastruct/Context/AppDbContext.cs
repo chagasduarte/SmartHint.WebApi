34563cd3e6641b326f01887a66d09b93d3e64c24
Chagas Duarte <70854311+chagasduarte@users.noreply.github.com>
2024-07-28 14:13:46 -0300

    Atualizar o AppDbContext.cs

--- SmartHint.Infrastruct/Context/AppDbContext.cs
+++ SmartHint.Infrastruct/Context/AppDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using SmartHint.Domain.Models;
 
@@ -8,13 +8,7 @@ namespace SmartHint.Infrastruct.Context
     {
         private IConfiguration _configuration;
         public AppDbContext(IConfiguration configuration, DbContextOptions<AppDbContext> options) : base(options) 
-        { 
-            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
-        }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseMySQL(_configuration.GetConnectionString("MySql"));
-        }
+        {}
         public DbSet<Cliente> Clientes { get; set; }
     }
 }
